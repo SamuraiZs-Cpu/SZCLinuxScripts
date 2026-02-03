e95663c5a0e1612b2644135056b437dd1d7ca06c
SamuraiZs-Cpu <marijeffers@gmail.com>
2026-02-03 18:41:20 -0500

    Refactor Usersetup.sh for username handling and file paths

--- Usersetup.sh
+++ Usersetup.sh
@@ -7,23 +7,26 @@ servera="student@servera"
 serverb="student@serverb"
 
 # get username.txt 
-read -r -p "Enter your username ('A-z9-0_'): " username
-if [[ $username =~ ^[A-za-z0-9_]+$ ]]; then
+if [ -z "username.txt" ]; then
+	read -r -p "Enter your username ('A-z9-0_'): " username
+elif [[ $username =~ ^[A-za-z0-9_]+$ ]]; then
 	echo "Hello, $nickname!"
-	echo $nickname > ~/.username.txt
+	echo $nickname > username.txt
 else
 	echo "Unauthorized characters detected. Exiting script"
 	exit 1
 fi
 
+
 sendsetupfiles(){
 #Send setup scripts to my servers
-scp ~/.username.txt $servera:~
-scp ~/SZCLinuxScripts/setupa.sh $servera:~
-scp ~/.username.txt $serverb:~
-scp ~/SZCLinuxScripts/setupb.sh $serverb:~
+scp username.txt $servera:~
+scp setupa.sh $servera:~
+scp username.txt $serverb:~
+scp setupb.sh $serverb:~
 }
 
 
 sendsetupfiles
+
 
